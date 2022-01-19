3c482fa47f048c414f40e677c760c81efc6029dd
Julien Legay <jlegay@fidme.com>
2022-01-19 17:00:38 +0100

    lint

--- src/jwt.js
+++ src/jwt.js
@@ -16,19 +16,12 @@ export class Jwt {
     return token.jws.JWS.sign(this.headers.alg, headers, payload, this.secret);
   }
 
-  static parseToken(token) {
+  static parseToken(tkn) {
     try {
-      var base64Url = token.split('.')[1];
-      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      var jsonPayload = decodeURIComponent(
-          atob(base64)
-              .split('')
-              .map(function(c) {
-                  return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-              })
-              .join(''),
-      );
-      
+      const base64Url = tkn.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`).join(''));
+
       return JSON.parse(jsonPayload);
     } catch (e) {
       console.log('[Sleek-Networking] parseToken', e);
