143217c9d1acb5f9ababb49887bea02274e6a401
Julien Legay <jlegay@fidme.com>
2022-01-19 11:54:05 +0100

    Add a parseToken function

--- src/jwt.js
+++ src/jwt.js
@@ -15,4 +15,24 @@ export class Jwt {
 
     return token.jws.JWS.sign(this.headers.alg, headers, payload, this.secret);
   }
+
+  static parseToken(token) {
+    try {
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(
+          atob(base64)
+              .split('')
+              .map(function(c) {
+                  return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+              })
+              .join(''),
+      );
+      
+      return JSON.parse(jsonPayload);
+    } catch (e) {
+      console.log('[Sleek-Networking] parseToken', e);
+      return false;
+    }
+  }
 }
